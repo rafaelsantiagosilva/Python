00df1b455c75259ec2dbbb73e7d4d772a3cb8275
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2023-10-06 21:38:09 -0300

    Importando só o necessário

--- 08_bibliotecas_modulos.py
+++ 08_bibliotecas_modulos.py
@@ -1,7 +1,7 @@
-import math
+from math import sqrt, ceil, floor
 print("{:-^30}".format('Aula 08'))
 numero = int(input("Digite um número:" ))
-raiz = math.sqrt(numero)
+raiz = sqrt(numero)
 print("Raíz quadrada: {}".format(raiz))
-print("Raiz arredondada para cima: {}".format(math.ceil(raiz)))
-print("Raiz arredondada para baixo: {}".format(math.floor(raiz)))
+print("Raiz arredondada para cima: {}".format(ceil(raiz)))
+print("Raiz arredondada para baixo: {}".format(floor(raiz)))

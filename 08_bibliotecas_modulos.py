17bfb84c536802626c215744fda2df1475d99445
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2023-10-06 21:36:37 -0300

    Testando algumas funções da biblioteca Math
    Testando math.sqrt(), math.ceil() e math.floor()

--- 08_bibliotecas_modulos.py
+++ 08_bibliotecas_modulos.py
@@ -1,2 +1,7 @@
+import math
 print("{:-^30}".format('Aula 08'))
-numero = input("Digite um número:" )
+numero = int(input("Digite um número:" ))
+raiz = math.sqrt(numero)
+print("Raíz quadrada: {}".format(raiz))
+print("Raiz arredondada para cima: {}".format(math.ceil(raiz)))
+print("Raiz arredondada para baixo: {}".format(math.floor(raiz)))

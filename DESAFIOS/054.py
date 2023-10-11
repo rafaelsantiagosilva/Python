5313fbb6c61f1a489e294034c93a8a767e0830d2
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2023-10-11 20:38:42 -0300

    Update 054.py

--- DESAFIOS/054.py
+++ DESAFIOS/054.py
@@ -1,11 +1,15 @@
-print('{:-^50}'.format('Exercício 53'))
-frase = str(input('Digite uma frase: \n')).strip().upper()
-frase = frase.split()
-frase_junta = ''.join(frase)
-frase_contraria = ''
-for i in range(len(frase_junta)-1, -1, -1):
-    frase_contraria+=frase_junta[i]
-if frase_junta == frase_contraria:
-    print('É um palíndromo')
-else:
-    print('Não é um palíndromo')
+import datetime
+print("{:-^50}".format('Exercício 54'))
+ano_atual = datetime.datetime.now().year
+maiores=0
+menores=0
+for i in range(0, 7):
+    ano_nasc = int(input('Digite o ano de nascimento da {}º pessoa: '.format(i+1)))
+    idade = ano_atual-ano_nasc
+    if idade >= 18:
+        print('É maior de idade: {}{}{} anos'.format('\033[32m', idade, '\033[m'))
+        maiores += 1
+    else:
+        print('Não é maior de idade: {}{}{} anos'.format('\033[31m', idade, '\033[m'))
+        menores += 1
+print('\nAo todo, tivemos {}{}{} menores e {}{}{} maiores de idade!'.format('\033[31m', menores, '\033[m', '\033[32m', maiores, '\033[m'))

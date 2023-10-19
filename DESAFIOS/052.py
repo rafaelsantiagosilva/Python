9c4c3dc338cf67385cd6aa8d65abe8c36c645265
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2023-10-18 22:10:11 -0300

    Update 052.py

--- DESAFIOS/052.py
+++ DESAFIOS/052.py
@@ -1,19 +1,23 @@
 import sys
 print('{:-^50}'.format('Exercício 52'))
-n = int(input('Digite um número: '))
 div = 0
-print('{:=^50}'.format('DIVISORES'))
-for i in range(1, n+1):
-    if n % i == 0:
-        div+=1
-        print('\033[1;34m', end="")
-        if div > 2:
-            resp = 'NÃO É PRIMO!'
+opc = 'S'
+while opc == 'S':
+    div = 0
+    print('\n{:=^50}'.format('DIVISORES'))
+    n = int(input('Digite um número: '))
+    for i in range(1, n+1):
+        if n % i == 0:
+            div+=1
+            print('\033[34m', end="")
+            if div > 2:
+                resp = 'NÃO É PRIMO!'
+            else:
+                resp = 'É PRIMO!'
         else:
-            resp = 'É PRIMO!'
-    else:
-        div+=0
-        print('\033[0;37m', end="")
-    sys.stdout.write('{} '.format(i, end=""))
-print('\n{}{:=^50}'.format('\033[m', ''))
-print('O número {} {}'.format(n, resp))
+            div+=0
+            print('\033[37m', end="")
+        sys.stdout.write('{} '.format(i, end=""))
+    print('\n{}{:=^50}'.format('\033[m', ''))
+    print('O número {} {}'.format(n, resp))
+    opc = str(input('\nQuer continuar? [S/N]\nR: ')).upper()

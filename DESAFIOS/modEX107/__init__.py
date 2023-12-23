831d6bc9c44232f56f0334e3ee4ebc546b160bda
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2023-12-23 18:42:28 -0300

    Modificando EX107

--- DESAFIOS/modEX107/__init__.py
+++ DESAFIOS/modEX107/__init__.py
@@ -1,11 +1,23 @@
 def metade(num):
+     '''
+     Calcula a metade do numero passado
+     '''
      return num/2
 
 def dobro(num):
+     '''
+     Calcula o dobro do numero passado
+     '''
      return num*2
 
 def aumentar(num):
+     '''
+     Calcula o numero + 10% dele mesmo
+     '''
      return num+(num/100*10)
 
 def diminuir(num):
+     '''
+     Calcula o numero - 10% dele mesmo
+     '''
      return num-(num/100*10)

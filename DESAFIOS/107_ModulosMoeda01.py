57e03f393f879f4ddd2caec2bcdd4e5629823899
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2023-12-23 18:39:36 -0300

    Exercício 107

---
+++ DESAFIOS/107_ModulosMoeda01.py
@@ -0,0 +1,7 @@
+import modEX107
+print(f"{"Exercício 107":=^80}")
+num = float(input("Digite um número: "))
+print("O seu dobro é", modEX107.dobro(num))
+print("A sua metade é", modEX107.metade(num))
+print("Aumentando em 10% fica", modEX107.aumentar(num))
+print("Diminuindo em 10% fica", modEX107.diminuir(num))
---
+++ DESAFIOS/modEX107/__init__.py
@@ -0,0 +1,11 @@
+def metade(num):
+     return num/2
+
+def dobro(num):
+     return num*2
+
+def aumentar(num):
+     return num+(num/100*10)
+
+def diminuir(num):
+     return num-(num/100*10)
